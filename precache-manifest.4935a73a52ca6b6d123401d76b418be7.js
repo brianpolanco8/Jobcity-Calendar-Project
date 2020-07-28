01a0849cd5afe3e14246147ad139baa9210764c7
brianf.baez8@gmail.com <brianf.baez8@gmail.com>
2020-07-28 11:21:35 -0400

    Updates

---
+++ precache-manifest.4935a73a52ca6b6d123401d76b418be7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5496e3960b684e9a901402fe26c2808f",
+    "url": "/Jobcity-Calendar-Project/index.html"
+  },
+  {
+    "revision": "6911bff98ff4236827f8",
+    "url": "/Jobcity-Calendar-Project/static/css/2.3838c02d.chunk.css"
+  },
+  {
+    "revision": "834066b0b2a33304edaf",
+    "url": "/Jobcity-Calendar-Project/static/css/main.6dec15f9.chunk.css"
+  },
+  {
+    "revision": "6911bff98ff4236827f8",
+    "url": "/Jobcity-Calendar-Project/static/js/2.244581f9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Jobcity-Calendar-Project/static/js/2.244581f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "834066b0b2a33304edaf",
+    "url": "/Jobcity-Calendar-Project/static/js/main.fd8e82b4.chunk.js"
+  },
+  {
+    "revision": "b476d7346aa662fb2bb4",
+    "url": "/Jobcity-Calendar-Project/static/js/runtime-main.4bad92e2.js"
+  }
+]);
