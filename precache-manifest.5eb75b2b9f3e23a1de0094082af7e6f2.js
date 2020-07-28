62a9791f54487336ad109e6cb8b4f284e67407ef
brianf.baez8@gmail.com <brianf.baez8@gmail.com>
2020-07-28 11:12:21 -0400

    Updates

---
+++ precache-manifest.5eb75b2b9f3e23a1de0094082af7e6f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a10692dd183b1096d503fcbe817772e",
+    "url": "/brianpolanco8.github.io/Jobcity-Calendar-Project/index.html"
+  },
+  {
+    "revision": "e841c937b65287ebfaff",
+    "url": "/brianpolanco8.github.io/Jobcity-Calendar-Project/static/css/2.3838c02d.chunk.css"
+  },
+  {
+    "revision": "8553f82f18ab83b62892",
+    "url": "/brianpolanco8.github.io/Jobcity-Calendar-Project/static/css/main.6dec15f9.chunk.css"
+  },
+  {
+    "revision": "e841c937b65287ebfaff",
+    "url": "/brianpolanco8.github.io/Jobcity-Calendar-Project/static/js/2.0dc4eefb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/brianpolanco8.github.io/Jobcity-Calendar-Project/static/js/2.0dc4eefb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8553f82f18ab83b62892",
+    "url": "/brianpolanco8.github.io/Jobcity-Calendar-Project/static/js/main.59c2064f.chunk.js"
+  },
+  {
+    "revision": "513a36c98d5a4827b5c4",
+    "url": "/brianpolanco8.github.io/Jobcity-Calendar-Project/static/js/runtime-main.5bbece01.js"
+  }
+]);
